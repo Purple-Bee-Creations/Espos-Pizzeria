6d47f855e188a183251d5aeb7f249ca28a4f7bb9
realmattbender <matt@bender.network>
2021-08-25 21:29:37 -0400

    delete
    delete all of it

--- wp-content/themes/victoria/slider/custom-slider.php
+++
@@ -1,9 +0,0 @@
-<div id="featured_slider">
-	<div class="slider_wrapper slider_stretched"> 
-		<?php 
-		
-		$slider = get_option( 'atp_customslider' ); 
-		echo do_shortcode($slider); 
-		?>
-	</div>
-</div>
